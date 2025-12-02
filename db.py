063ab27a282b9bf1b6db4d61ffd3cb5d7cdedc04
v-coder-cmd <v.amancio@aluno.senai.br>
2025-12-02 20:19:03 -0300

    TA ON KRL

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="root",
+        password="Victor@!2020",
         database="chatdb"
     )
